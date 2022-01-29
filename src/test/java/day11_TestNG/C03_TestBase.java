fc2f28d04001ef697637b727efbf824851b2838b
tallboy6077 <tallboy6077@gmail.com>
2022-01-30 00:37:52 +0300

    day11_TestNG

---
+++ src/test/java/day11_TestNG/C03_TestBase.java
@@ -0,0 +1,20 @@
+/* package day11_TestNG;
+
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+public class C03_TestBase extends TestBase {
+
+
+    @Test
+    public void test01(){
+        driver.get("https://www.amazon.com");
+
+
+    }
+
+
+}
+
+
+ */
