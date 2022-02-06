5081e994a9b59c70ce3b7c28f26ce24a2df73652
tallboy6077 <tallboy6077@gmail.com>
2022-02-06 21:11:45 +0300

    SeleniumPractise

--- src/test/java/SeleniumPractise/Question_4.java
+++ src/test/java/SeleniumPractise/Question_4.java
@@ -2,12 +2,13 @@ package SeleniumPractise;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
 public class Question_4 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","src/driver/chromedriver.exe");
         WebDriver driver=new ChromeDriver();
         driver.manage().window().maximize();
@@ -25,20 +26,25 @@ public class Question_4 {
          */
 
 
+
         // Navigate to  https://testpages.herokuapp.com/styled/index.html
         driver.get("https://testpages.herokuapp.com/styled/index.html");
         // Click on  Calculate under Micro Apps
         driver.findElement(By.id("calculatetest")).click();
         //    Type any number in the first input
-        driver.findElement(By.id("number1")).sendKeys("5");
+        driver.findElement(By.id("number1")).sendKeys("20");
+        Thread.sleep(2000);
+
+        driver.findElement(By.id("function")).click();
         //    Type any number in the second input
         driver.findElement(By.id("number2")).sendKeys("7");
         // Click on Calculate
         driver.findElement(By.id("calculate")).click();
         // Get the result
-        driver.findElement(By.id("answer")).getText();
+
+        driver.findElement(By.xpath("//span[@id='answer']")).getText();
 
         // close
-        driver.quit();
+       driver.quit();
     }
 }
