967637acf1c9fff5d7e19e86aca203cc382afb13
tallboy6077 <tallboy6077@gmail.com>
2022-02-27 08:20:19 +0300

    JavaScriptExecuter

--- src/test/java/SeleniumENGLISH/Day08_JavaScriptExecuter.java
+++ src/test/java/SeleniumENGLISH/Day08_JavaScriptExecuter.java
@@ -1,5 +1,6 @@
 package SeleniumENGLISH;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
@@ -8,7 +9,7 @@ import utilities.TestBase;
 
 public class Day08_JavaScriptExecuter extends TestBase {
 
-    //it is coming from SELENIUM, used for better interaction.
+    //it is coming from SELENIUM, used for better interaction. **** IT IS USED COMMON***
     // WE CAN SCROLL CERTAIN SPECIFIC ELEMENT WITH JAVA SCRIPT EXECUTER!!!
     @Test
     public void scrollIntoView() throws InterruptedException {
@@ -25,6 +26,41 @@ public class Day08_JavaScriptExecuter extends TestBase {
        WebElement ourRooms=driver.findElement(By.xpath("//*[.='Our Rooms']"));
       //  STEP 2 => Use jAVASCRIPTexecute method.
         je.executeScript("arguments[0].scrollIntoView(true);",ourRooms);
+
+
+    }
+    @Test
+    public void clickWithJavaScripts(){
+        driver.get("https://a.testaddressbook.com/sign_in");
+
+        //Click onsign in button using Java Script Executer
+
+      WebElement  signInButton=driver.findElement(By.xpath("//input[@type='submit']"));
+      //  signInButton.click(); Normally, click() method works with selenium.We use this when it is good.
+        //Some elements can be challenging to click
+        //in that case, we have javascriptexecuter click as an alternative.
+        //THIS PERFORMS BETTER ON SOME APPLICATIONS.
+
+        //1. Create Javascript executer object
+        JavascriptExecutor je=(JavascriptExecutor) driver;
+
+        //2. Use the appropriate method
+        je.executeScript("arguments[0].click();",signInButton);
+
+        //Assert that the click happened using the message.
+        Assert.assertTrue(driver.getPageSource().contains("Bad email or password."));
+
+
+
+    }
+    @Test
+    public void scrollDownWithJavaScript() throws InterruptedException {
+        driver.get("https://www.carettahotel.com/");
+
+        // Scroll down to the page using javascript executer.
+        JavascriptExecutor je=(JavascriptExecutor) driver;
+        Thread.sleep(3000);
+        je.executeScript("window.scrollTo(0,document.body.scrollHeight)");
 
 
     }
