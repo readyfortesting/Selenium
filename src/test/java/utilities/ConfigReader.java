bb2e035b61d6b863e721ab60bb72dded1d507e04
tallboy6077 <tallboy6077@gmail.com>
2022-03-03 15:22:05 +0300

    Configuration Reader

--- src/test/java/utilities/ConfigReader.java
+++ src/test/java/utilities/ConfigReader.java
@@ -1,24 +1,28 @@
 package utilities;
 
+import org.testng.annotations.Test;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigReader {
-
+// This class will only be used for getting the data from configuration.properties file
     public static Properties properties;
 
     static {
-
+            //path of the config file
         String path="src/configuration.properties";
 
         try {
-
-            FileInputStream fis=new FileInputStream(path);
+                //Opening the file
+            FileInputStream file=new FileInputStream(path);
+                //Loading the file
             properties=new Properties();
-            properties.load(fis);
-            fis.close();
+            properties.load(file);
+             //Closing the file
+            file.close();
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -27,11 +31,20 @@ public class ConfigReader {
 
     }
 
-
+  // This method accepts the key and returns the value
     public static String getProperty(String key){
 
         return properties.getProperty(key);
     }
-
-
 }
+
+/*
+    //Test if it works ?
+@Test
+    public void test1(){
+    System.out.println(getProperty("HMCUrl")); // https://www.hotelmycamp.com/
+    System.out.println(getProperty("TechproUrl")); // https://www.techproeducation.com
+}
+
+ */
+
