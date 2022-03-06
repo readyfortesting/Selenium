9a69e94432d169d1f6355c57273b8548246bf363
tallboy6077 <tallboy6077@gmail.com>
2022-03-06 14:54:26 +0300

    Day02_Locators

--- src/test/java/SeleniumENGLISH/Day02_Locators.java
+++ src/test/java/SeleniumENGLISH/Day02_Locators.java
@@ -1,10 +1,11 @@
  package SeleniumENGLISH;
- import org.junit.Assert;
- import org.junit.Test;
+
  import org.openqa.selenium.By;
  import org.openqa.selenium.WebDriver;
  import org.openqa.selenium.WebElement;
  import org.openqa.selenium.chrome.ChromeDriver;
+ import org.testng.Assert;
+ import org.testng.annotations.Test;
 
  public class Day02_Locators {
 
